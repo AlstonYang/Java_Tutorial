eb46e9f86be7b5a1a163eb67b63538497250542a
alston <alston@100.76.49.45>
2021-03-10 15:05:05 +0800

    initial

---
+++ 109356003_OOP_Lab01/src/HelloTest.java
@@ -0,0 +1,14 @@
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Greetings from Ren Han, Yang!");
+		
+		System.out.print("Greetings from Ren Han, Yang!");
+		System.out.printf("Greetings from %s \n","Ren Han, Yang");
+		
+		System.out.printf("Greetings from %s\n","Ren Han, Yang");
+	}
+
+}
